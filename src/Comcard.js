7a8516ba3cd44095083125d01f8bf14e65438b63
l552177239 <552177239@qq.com>
2017-08-17 15:17:49 +0000

    cssTransition

--- src/Comcard.js
+++ src/Comcard.js
@@ -5,13 +5,23 @@ class Comcard extends React.Component {
   constructor(){
     super()
     this.state = {
+      item: [1,2,3,4],
       show: true
     }
   }
   handleClick = () => console.log(1)
   render(){
+    const items = this.state.item.map( item => (
+      <div key={item}>{item}</div>
+    ))
     return(
       <div className="comcard">
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          {items}
+        </CSSTransitionGroup>
         <button onClick={this.handleClick}>点击</button>
       </div>
     )
